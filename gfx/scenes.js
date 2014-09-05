daadad35d8171a7b56df098c0e1d2d866a4c10ca
Nicolas Silva <nical.silva@gmail.com>
2014-09-05 21:37:04 +0200

    my eyes hurt

--- gfx/scenes.js
+++ gfx/scenes.js
@@ -14,13 +14,14 @@ function draw_mesh_list(list) {
 }
 
 function shift_param_buffer(array, new_val_y0, new_val_y1, decay) {
-    var param_buf = gfx.geometries.param_buffers[0].param_buffer;
-    for (var i = 1; i < param_buf.length/2; ++i) {
-        param_buf[i*2]   = param_buf[(i-1)*2] * decay;
-        param_buf[i*2+1] = param_buf[(i-1)*2+1] * decay;
+    for (var i = 1; i < array.length/2; ++i) {
+        var d1 = array[(i-1)*2] - array[i*2];
+        var d2 = array[(i-1)*2+1] - array[i*2+1];
+        array[i*2]   += d1 * decay;
+        array[i*2+1] += d2 * decay;
     }
-    param_buf[0] = new_val_y0;
-    param_buf[1] = new_val_y1;
+    array[0] = new_val_y0;
+    array[1] = new_val_y1;
 }
 
 function scenes_init() {
@@ -36,11 +37,18 @@ function scenes_init() {
                     render: draw_mesh_list(gfx.geometries.param_buffers, DISABLE_DEPTH_TEST),
                     program: gfx.programs.lines1,
                     update: function(times) {
-                        var param_buf = gfx.geometries.param_buffers[0].param_buffer;
-                        var t = times.demo;
-                        shift_param_buffer(param_buf, Math.sin(t/10), Math.sin(t/10)*3.0, 0.8);
-                        upload_param_buffer(gfx.geometries.param_buffers[0]);
-                        gfx.uniforms.u_float_param_0 = 0.2 + 0.1*Math.sin(t/12.0);
+                        for (var i = 0; i < gfx.geometries.param_buffers.length; ++i) {
+                            var param_buf = gfx.geometries.param_buffers[i].param_buffer;
+                            var t = times.demo;
+                            var twist = 5*Math.sin(t/20)
+                            shift_param_buffer(param_buf,
+                                2*twist+2*twist*Math.sin(t/10),
+                                2*(twist+2)+2*(twist+2)*Math.sin(t/10),
+                                0.3
+                            );
+                            upload_param_buffer(gfx.geometries.param_buffers[i]);
+                        }
+                        gfx.uniforms.u_float_param_0 = 0.2 + 0.1*Math.sin(t/(i+2));
                     }
                 },
             ]
