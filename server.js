65af6ae058be8d6349fa5f29fdb9132d7839e2de
Cgg <clement.geiger@gmail.com>
2014-09-05 16:42:13 +0200

    shutup jshint

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var midi = require('midi');
- var osc = require('node-osc');
+var osc = require('node-osc');
 
 var input = new midi.input();
 var count = input.getPortCount();
 for (var i = 0; i < count; i++) {
   console.log(input.getPortName(i));
 }
-// input.openPort(0);
 input.openPort(1);
 
 input.on('message', function(deltaTime, message) {
@@ -16,7 +15,7 @@ input.ignoreTypes(false, false, false);
 
 var oscServer = new osc.Server(3333, '0.0.0.0');
 oscServer.on("message", function (msg, rinfo) {
-  console.log("TUIO message:"); console.log(msg);
+  console.log("TUIO message:", msg);
 });
 
 var readline = require('readline');
