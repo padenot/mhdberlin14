6df5805fa799c39907aa87d76ac2ddf3929b4591
Paul Adenot <paul@paul.cx>
2014-09-06 00:09:20 +0200

    perc baking

--- sub.js
+++ sub.js
@@ -1,6 +1,6 @@
-sub.prototype = extend(Instrument);
+subsub.prototype = extend(Instrument);
 
-function sub(ctx) {
+function subsub(ctx) {
   Instrument.call(this, ctx);
 
   this.osc = ac.createOscillator();
@@ -10,26 +10,29 @@ function sub(ctx) {
 
   this.register_param("attack", 0.01, 0.0, 0.1, 0.01);
   this.register_param("decay", 2.0, 0.0, 4.0, 0.1);
-  this.register_param("sustain", 0.6, 0.0, 1.0, 0.1);
+  this.register_param("sustain", 0.6, 0.0, 1.0, 0.0);
 
   this.osc.connect(this.gain);
 
   this.osc.start();
+  this.name = function() {
+    return "subsub";
+  }
 }
 
-sub.prototype.trigger = function(note, velocity, time) {
+subsub.prototype.name = function() {
+  return "subsub";
+}
+
+subsub.prototype.trigger = function(note, velocity, time) {
   this.osc.frequency.setValueAtTime(n2f(note), ac.currentTime);
   this.gain.gain.cancelScheduledValues(ac.currentTime);
   this.gain.gain.setValueAtTime(0.0, ac.currentTime);
   this.gain.gain.linearRampToValueAtTime(velocity2gain(velocity) * 0.8, ac.currentTime + this.p("attack"));
   this.gain.gain.setTargetAtTime(this.p("sustain") / 2, ac.currentTime + this.p("attack"), this.p("decay"));
-  this.gain.gain.setTargetAtTime(0.0, ac.currentTime + this.p("attack") + this.p("decay"), 0.3);
+  this.gain.gain.setTargetAtTime(0.0, ac.currentTime + this.p("attack"), this.p("decay"));
 }
 
-sub.prototype.name = function() {
-  return "sub";
-}
-
-sub.prototype.connect = function(node) {
+subsub.prototype.connect = function(node) {
   this.gain.connect(node);
 }
