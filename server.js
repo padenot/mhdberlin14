3d235ca526ba9a47e3bf5a9825c59ce666800250
Cgg <clement.geiger@gmail.com>
2014-09-05 18:02:14 +0200

    IMPLEMENT BASIC MONOME HANDLING

--- server.js
+++ server.js
@@ -1,28 +1,53 @@
+var instrumentsColCount = 2;
+var instruments = [];
+for(var i = 0; i < instrumentsColCount * 7; ++i) {
+  instruments.push(false);
+}
+
+// MONOME
+require('monode')().on('connect', function(device) {
+  console.log('monome status: ONLINE');
+
+  device.on('key', function(x, y, i) {
+    if (i === 0) {
+      return;
+    }
+
+    // instrument selection
+    if (x < instrumentsColCount) {
+      console.log('set instrument', y + (x * 8));
+      instruments[y + (x * 8)] = !instruments[y + (x * 8)];
+      updateInstrumentsLeds();
+    }
+    else if (x >= instrumentsColCount) {
+      console.log('play!');
+    }
+  });
+
+  function updateInstrumentsLeds() {
+    var i;
+    for (i = 0; i < instruments.length; ++i) {
+      device.led(i/8, i % 8, instruments[i]);
+    }
+  }
+});
+
+// NANOKEY
 var midi = require('midi');
-var osc = require('node-osc');
 
 var input = new midi.input();
+
 var count = input.getPortCount();
+console.log('ONLINE midi controllers:');
 for (var i = 0; i < count; i++) {
-  console.log(input.getPortName(i));
+  console.log('\t' + input.getPortName(i));
 }
-input.openPort(1);
+if (count > 1) {
+  input.openPort(1);
+}
 
 input.on('message', function(deltaTime, message) {
-  console.log('m:' + message + ' d:' + deltaTime);
+  console.log('midi message', message);
+  // send websocket message
 });
 input.ignoreTypes(false, false, false);
-
-var oscServer = new osc.Server(3333, '0.0.0.0');
-oscServer.on("message", function (msg, rinfo) {
-  console.log("TUIO message:", msg);
-});
-
-var readline = require('readline');
-
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
-
